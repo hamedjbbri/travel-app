decf21dc2a698480e9af4390a47a505df5f21a86
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-09-30 10:17:54 -0700

    Update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';  
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello, world</h1>
+        </div>
+    );
+}
+
+export default App;
